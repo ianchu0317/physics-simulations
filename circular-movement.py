d3a50471c01e8e8c9b51ab39e1c46675096c23ea
Ian Chen <chenian317@gmail.com>
2024-05-29 17:53:05 -0300

    added plotting and reset system

--- circular-movement.py
+++ circular-movement.py
@@ -3,7 +3,7 @@ El presente programa simula movimientos circulares.
 """
 
 import pygame
-from math import cos, sin, pi, radians
+from math import cos, sin, pi, radians, degrees
 from matplotlib import pyplot as plt
 
 # Iniciar módulos
@@ -40,23 +40,25 @@ def convert_to_degree(radiant):
 
 # Clase de la partícula
 class Particle:
-    def __init__(self, colour, vel0deg):
+    def __init__(self, colour, vel0deg, accel_ang, r):
         # Apariencia de la partícula
         self.radius = 10  # px
         self.colour = colour
         # Características del movimiento circular
-        self.RADIUS = 100  # px
+        self.RADIUS = r  # px
         self.circ_centerx = WIDTH/2
         self.circ_centery = HEIGHT/2
         self.circ_center = (self.circ_centerx, self.circ_centery)
         # Ecuaciones del movimiento circular
         # Ecuaciones circulares
-        self.accel_ang = 0
+        self.accel_ang = accel_ang
         self.vel0ang = radians(vel0deg)  # rad/s
         self.vel_ang = self.vel0ang
         self.angular0 = 0  # radianes
         self.angular = self.angular0
         # Ecuaciones tangenciales
+        self.accel_tan = self.RADIUS*self.accel_ang
+        self.vel_tan = self.RADIUS*self.vel_ang
         # Eje Y
         self.pos0x = WIDTH/2 + self.RADIUS
         self.pos_x = self.pos0x
@@ -70,6 +72,9 @@ class Particle:
         self.vel_ang = self.vel0ang + self.accel_ang*TIME
         self.angular = self.angular0 - self.vel0ang*TIME + (self.accel_ang/2)*pow(TIME, 2)
         # Actualizar ecuaciones tangenciales (de posición)
+        self.accel_tan = self.RADIUS*self.accel_ang
+        self.vel_tan = self.RADIUS*self.vel_ang
+        # Posiciones
         self.pos_y = self.circ_centery + self.RADIUS*sin(self.angular)
         self.pos_x = self.circ_centerx + self.RADIUS*cos(self.angular)
 
@@ -80,7 +85,39 @@ class Particle:
         pygame.draw.circle(screen, self.colour, (self.pos_x, self.pos_y), self.radius)
 
 
-p1 = Particle('red', 50)
+# Resetear simulación
+def reset(particle):
+    global TIME
+    TIME = 0
+    # Reset velocity and positions
+    particle.pos_x = particle.pos0x
+    particle.pos_y = particle.pos0y
+
+
+# Imprimir información en pantalla
+def plot(particle):
+    text_y_pos = 20
+    text_x_pos = 550
+    # Tabla de información
+    datas = [[':****INFORMACIÓN****', ''],
+             ['Tiempo (s)', round(TIME, 2)],
+             ['Posición X (px)', round(particle.pos_x, 2)],
+             ['Posición Y (px)', round(particle.pos_y, 2)],
+             [':--Información angular--', ''],
+             ['Aceleración angular(rad/s2): ', particle.accel_ang],
+             ['Velocidad angular (rad/s): ', round(particle.vel_ang, 2)],
+             ['Posición angular (grados)', f'{round(degrees(particle.angular), 1)}º'],
+             [':--Información tangencial--', ''],
+             ['Aceleración tangencial (px/s2): ', round(particle.accel_tan, 2)],
+             ['Velocidad tangencial (px/s): ', round(particle.vel_tan, 2)]]
+    # Renderizar información
+    for info, data in datas:
+        text = font.render(f'{info}: {data}', False, 'white')
+        screen.blit(text, (text_x_pos, text_y_pos))
+        text_y_pos += 20
+
+
+p1 = Particle('red', 50, -0.01, 100)
 
 # Bucle principal de simulación
 while run:
@@ -92,6 +129,7 @@ while run:
     p1.update()
 
     # imprimir información
+    plot(p1)
 
     # Actualizar el tiempo y movimiento
     TIME += dt
@@ -100,7 +138,12 @@ while run:
         # Salir programa
         if event.type == pygame.QUIT:
             run = False
-
+        # Resetear programa
+        if event.type == pygame.KEYDOWN:
+            # Resetear programa con datos actuales
+            if event.key == pygame.K_ESCAPE:
+                reset(p1)
+            # Resetear simulación cambiando datos del ángulo de tiro
     pygame.display.flip()
 pygame.quit()
 quit()
